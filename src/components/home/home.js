745f8973a612fd067db875ea0d7a6518fb278d93
ZaraiBilel <zaraibilel1@gmail.com>
2023-01-29 14:43:08 +0100

    archit body

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -2,9 +2,25 @@ import './style.scss';
 
 const Home = () => {
   return (
-<div>
-  
-</div>
+    <div>
+      <div className='body'>
+        
+
+        <div className='leftSideBody'>
+          <div className='experience'>
+
+
+          </div>
+          <div className='education'>
+
+          </div>
+        </div>
+        <div className='rightSideBody'>
+
+
+        </div>
+      </div>
+    </div>
   )
 }
 export default Home;
