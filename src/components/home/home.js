ab07ec3fec59ca8a0baedf61d80384d4d4117ccc
ZaraiBilel <zaraibilel1@gmail.com>
2023-01-29 16:46:13 +0100

    update archit body

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -4,22 +4,32 @@ const Home = () => {
   return (
     <div>
       <div className='body'>
-        
 
-        <div className='leftSideBody'>
-          <div className='experience'>
+        <div className='cv'> </div>
+        <div>
+          <div className='leftSideBody'>
+            <div className='experience'>
+
+
+            </div>
+            <div className='education'>
+
+            </div>
+          </div>
+          <div className='rightSideBody'>
+            <div className='certification'>
+
+            </div>
+            <div className='project'>
+
+            </div>
 
 
           </div>
-          <div className='education'>
-
-          </div>
-        </div>
-        <div className='rightSideBody'>
-
-
         </div>
       </div>
+      <div className='technicalCompetence'></div>
+      <div className='professionalCompetence'></div>
     </div>
   )
 }
