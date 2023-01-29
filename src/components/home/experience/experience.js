15bd4f2a7f0493b16411eb63839fdf784f790cca
ZaraiBilel <zaraibilel1@gmail.com>
2023-01-29 16:01:51 +0100

    architect exp

--- src/components/home/experience/experience.js
+++ src/components/home/experience/experience.js
@@ -5,12 +5,57 @@
 
 import './style.scss';
 
-const Experience = () => {
+const Experience = (experience) => {
   return (
- <>
- 
- 
- </>
+    <div className='experience'>
+    <div className='poste'> {experience.poste}</div>
+
+    <div className='duration'> <div className='duree'> {experience.dateDebut} - {experience.dateSortie}</div> <div className='societe'>{experience.societe}</div></div>
+
+    <div>
+      <div>
+        {experience?.taches?.map((el, i) => (
+          <>
+            <div key={i} className='tache' >
+
+              <div className='tache-icon'> {el.icon} </div>
+              <div className='tache-descriptionTache'> {el.tache}    </div>
+              
+            </div>
+          </>
+
+        )
+
+
+        )}
+        {experience.poste}
+
+      </div>
+
+    </div>
+
+  </div>
     )
   }
   export default Experience;
+  Experience.defaultProps = {
+    experience: {
+      poste: 'ing info',
+      dateDebut: '2019',
+      dateSortie: '2020',
+      nameSociete: 'anime',
+  
+      taches: [{
+        id: 1,
+        icon:'',
+        tache: 'cree script',
+      },
+      {
+        id: 2,
+        icon:'',
+        tache: 'animation'
+      }]
+  
+    }
+  
+  }
