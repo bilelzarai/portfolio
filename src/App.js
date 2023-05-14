fcaf8cf2c451a0e01b64d8d62bc3d24f38818266
zaraibilel <zaraibilel1@gmail.com>
2023-05-14 22:48:32 +0100

    feat: test

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 // import './App.css';
 // import Footer from './components/footer/footer';
-import Navbar from './components/navbar/navbar';
+// import Navbar from './components/navbar/navbar';
 import './App.scss'
-import Candidate from './components/candidats/candidate';
+import SignIn from './components/sign-in/sign-in';
+// import Candidate from './components/candidats/candidate';
 // import SignIn from './components/sign-in/sign-in';
 // import Home from './components/home/home';
 function App() {
   return (
     <div className="app">
-      <Navbar className="navbar" />
+      {/* <Navbar className="navbar" /> */}
       test
-      <Candidate className="app-sign" />
+      <SignIn/>
+      {/* <Candidate className="app-sign" /> */}
       {/* <header className="App-header">
         <Home/>
         <Footer/>
